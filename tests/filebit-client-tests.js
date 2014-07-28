967bf423e6dac2561521cb0a3161fb30c5da8433
Michal Budzynski <michal@virtualdesign.pl>
2014-07-28 19:20:54 +0200

    [FILEBIT-CLIENT] - :snowman: wrong login tests

--- tests/filebit-client-tests.js
+++ tests/filebit-client-tests.js
@@ -26,3 +26,17 @@ fbc.login(login, password, function(err, resp){
   assert.equal(err, null);
   assert(resp);
 });
+
+// wrong password
+mockLogin('Podane hasło jest nieprawidłowe!');
+fbc.login(login, password, function(err, resp) {
+  assert.equal(err.message, 'Login error!');
+  assert.equal(resp, undefined);
+});
+
+// wrong login
+mockLogin('Błąd: Podany użytkownik nie istnieje! Niezła lipa.');
+fbc.login(login, password, function(err, resp) {
+  assert.equal(err.message, 'Login error!');
+  assert.equal(resp, undefined);
+});
